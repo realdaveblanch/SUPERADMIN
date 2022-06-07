411569db5fac6d5737ed3d0bc542ec4b192829f7
realdaveblanch <d.rodriguezp@outlook.es>
2022-06-07 12:06:16 +0200

    activar desactivar funcionando corectamente

--- backend/u/reactivar.php
+++ backend/u/reactivar.php
@@ -12,10 +12,29 @@ $mystring = $clon;
 // Test if string contains the word 
 if(strpos($mystring, $word) !== false){
 	$a = substr($clon, 0, -4);
-	echo $a;
 	rename($clon, $a );
+//QUITAR PUT CONTENTS	
+	
+	
+	$key = $a;
+$contents = '';
+$fc=file("cfg/urlunactive.ini");
+ foreach($fc as $line)
+  {
+    if (!strstr($line,$key))
+    {
+       $contents .= $line; 
+     }  
+  }
+  file_put_contents('cfg/urlunactive.ini',$contents);
+  $fp4 = fopen('cfg/urlselect.ini', 'a');
+	fwrite($fp4, $a);
+  
+  
+	echo "<META http-equiv=".'"REFRESH"'." CONTENT=".'"2;URL=ok.php"'.">";
 } else{
     echo "No está inactivo!";
+	echo "<META http-equiv=".'"REFRESH"'." CONTENT=".'"2;URL=fail.php"'.">";
 }
 
 
--- backend/u/rename.php
+++ backend/u/rename.php
@@ -4,9 +4,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 		
 		$clon = $_POST['clones'];	
 rename($clon, $clon . "_old");
+//Se escribe en urlunactive, que este clon ha sido desactivado
 $fp4 = fopen('cfg/urlunactive.ini', 'a');
-		file_put_contents('cfg/urlunactive.ini','', LOCK_EX);
 	fwrite($fp4, $clon . "_old");
+	
+//Se busca la línea en el select y se elimina
+
+	$key = $clon;
+$contents = '';
+$fc=file("cfg/urlselect.ini");
+ foreach($fc as $line)
+  {
+    if (!strstr($line,$key))
+    {
+       $contents .= $line; 
+     }  
+  }
+  file_put_contents('cfg/urlselect.ini',$contents);
+  
+	echo "<META http-equiv=".'"REFRESH"'." CONTENT=".'"0;URL=ok.php"'.">";
+}
+else {
+	echo "<META http-equiv=".'"REFRESH"'." CONTENT=".'"0;URL=fail.php"'.">";
 }
 
+
+ 
 ?>
